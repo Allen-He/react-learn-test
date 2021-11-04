b01f03bfcfe549d5ab8e9c72c3d3b054a023ef63
Allen-He <2426567439@qq.com>
2021-11-04 18:43:22 +0800

    01_简易轮播图

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import img1 from './assets/01.jpeg';
+import img2 from './assets/02.jpeg';
+import img3 from './assets/03.jpeg';
+import './assets/index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+const imgArr = [img1, img2, img3];
+let curIndex = 0;
+let timer = null;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/** 简易轮播图 */
+function render() {
+  const banner = (
+    <div className="bannerBox">
+      <img src={imgArr[curIndex]} alt="" />
+    </div>
+  )
+  ReactDOM.render(banner, container);
+}
+
+function start() {
+  stop();
+  timer = setInterval(() => {
+    curIndex = (curIndex + 1) % imgArr.length;
+    render();
+  }, 1000);
+}
+function stop() {
+  clearInterval(timer);
+}
+
+start();
+
+container.onmouseenter = function () {
+  stop();
+}
+container.onmouseleave = function () {
+  start();
+}
