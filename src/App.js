c9c3973d05c420de7c552f77e9767c658417180a
Allen-He <2426567439@qq.com>
2021-11-27 21:03:30 +0800

    16_实现Vue路由模式（routeConfig.js、RootRouter.js、BetterLink.js）

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
-import FadeTransitionTest from './components/FadeTransition/Test'
+import { BrowserRouter as Router } from "react-router-dom"
+import Link from './BetterLink'
+import RootRouter from "./RootRouter";
+import "./App.css"
 
 export default function App() {
   return (
-    <div>
-      <FadeTransitionTest/>
-    </div>
+    <Router>
+      <nav>
+        {/* <Link to='/'>首页</Link>
+        <Link to='/news'>新闻页</Link> */}
+        <Link to={{name:"home"}}>首页</Link>
+        <Link to={{name:"news"}}>新闻页</Link>
+      </nav>
+      <div>
+        {/* 匹配网站的顶级页面 */}
+        <RootRouter />
+      </div>
+    </Router>
   )
 }
